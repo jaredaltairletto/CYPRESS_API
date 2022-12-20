359c73479b584283200842f8275318edc8d77963
Jared <dimak2test@gmail.com>
2022-12-20 16:01:47 +0200

    put method

---
+++ cypress/e2e/USER_PUT.cy.js
@@ -0,0 +1,34 @@
+const { email } = require('../support/Faker');
+const dataJson = require('../fixtures/MyBody');
+
+describe('get api user test', () => {
+    let accesToken = '72e78981270b629bd33b2bd5dcb2503d8ba161df413eddc6434c2d90fcbbda8d'
+    it('get users', () => {
+         cy.request({
+            method : 'GET',
+            url : 'https://gorest.co.in/public/v2/users',
+            headers : {
+                'Authorization' : 'Bearer ' + accesToken
+            }         
+         }).then((response)=>{
+            const ID_ToUPD = response.body[3].id
+            cy.request({
+                method : 'PUT',
+                url : 'https://gorest.co.in/public/v2/users/' + ID_ToUPD,
+                headers : {
+                    'Authorization' : 'Bearer ' + accesToken
+                },
+                body:{
+                    "email": email,
+                    "name": "UPDATED_USER",
+                    "gender": dataJson.gender, 
+                    "status": dataJson.status
+                } 
+            })
+            .then((response)=>{
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.property('name', 'UPDATED_USER') 
+            })
+         })
+    });
+});
